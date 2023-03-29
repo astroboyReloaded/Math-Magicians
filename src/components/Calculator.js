42e29d6aa61619ec766233c4fd9a08f3ed91f46d
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-03-29 14:49:00 -0600

    Screen displays the state object

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,24 @@
-import Screen from './screen';
+// import { useState } from 'react';
+// import Screen from './screen';
 import Keypad from './keypad';
+// import operate from '../logic/operate';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <Screen />
-    <Keypad />
-  </div>
-);
+const Calculator = () => {
+  const state = {
+    total: '3',
+    next: '6',
+    operation: '+',
+  };
+
+  return (
+    <div className="calculator">
+      <div className="Screen">
+        {`${state.total + state.operation + state.next}`}
+      </div>
+      <Keypad onClick={() => calculate(state)} />
+    </div>
+  );
+};
 
 export default Calculator;
--- src/components/screen.js
+++ src/components/screen.js
@@ -1,3 +1,5 @@
-const Screen = () => <div className="Screen">0</div>;
+// const Screen = ({ total, next, operation }) => (
+//   <div className="Screen">{(total, operation, next)}</div>
+// );
 
-export default Screen;
+// export default Screen;

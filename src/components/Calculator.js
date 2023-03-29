470b055817dbd7341360c7cd2cc269946bc0b72e
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-03-29 09:54:00 -0600

    Create Screen + Keypad as nested components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,65 +1,10 @@
+import Screen from './screen';
+import Keypad from './keypad';
+
 const Calculator = () => (
   <div className="calculator">
-    <div className="Screen">0</div>
-    <div className="Keys">
-      <button type="button" className="key">
-        AC
-      </button>
-      <button type="button" className="key">
-        +/-
-      </button>
-      <button type="button" className="key">
-        %
-      </button>
-      <button type="button" className="key key-operator">
-        ÷
-      </button>
-      <button type="button" className="key">
-        7
-      </button>
-      <button type="button" className="key">
-        8
-      </button>
-      <button type="button" className="key">
-        9
-      </button>
-      <button type="button" className="key key-operator">
-        ×
-      </button>
-      <button type="button" className="key">
-        4
-      </button>
-      <button type="button" className="key">
-        5
-      </button>
-      <button type="button" className="key">
-        6
-      </button>
-      <button type="button" className="key key-operator">
-        -
-      </button>
-      <button type="button" className="key">
-        1
-      </button>
-      <button type="button" className="key">
-        2
-      </button>
-      <button type="button" className="key">
-        3
-      </button>
-      <button type="button" className="key key-operator">
-        +
-      </button>
-      <button type="button" className="key  zero">
-        0
-      </button>
-      <button type="button" className="key">
-        .
-      </button>
-      <button type="button" className="key key-operator">
-        =
-      </button>
-    </div>
+    <Screen />
+    <Keypad />
   </div>
 );
 

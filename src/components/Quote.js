3cd2befd40f34146e223bee6952a47b3f2f864b3
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-03-30 14:46:40 -0600

    fetch from API, render quote in container

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,3 +1,29 @@
-const Quote = () => <section>Quote</section>;
+import { useState, useEffect } from 'react';
+
+const Quote = () => {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    const API_KEY = 'oNaTrFfgL5OH2S52NR+xew==tPr1dJuqTaePURP8';
+    const category = 'knowledge';
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch(
+          `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+          {
+            method: 'GET',
+            headers: { 'X-Api-Key': API_KEY },
+            contentType: 'application/json',
+          },
+        );
+        const data = await response.json();
+        setQuote(data[0].quote);
+      }
+    };
+    fetchQuote();
+  }, []);
+
+  return <section className="quoteContainer">{quote}</section>;
+};
 
 export default Quote;

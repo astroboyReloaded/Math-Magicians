3bbe32c62f7a24d032ec173d686eb4003f8b074d
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-03-30 15:05:33 -0600

    Inform loading + handle error

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react';
 
 const Quote = () => {
   const [quote, setQuote] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     const API_KEY = 'oNaTrFfgL5OH2S52NR+xew==tPr1dJuqTaePURP8';
     const category = 'knowledge';
     const fetchQuote = async () => {
+      setIsLoading(true);
       try {
         const response = await fetch(
           `https://api.api-ninjas.com/v1/quotes?category=${category}`,
@@ -18,12 +21,21 @@ const Quote = () => {
         );
         const data = await response.json();
         setQuote(data[0].quote);
+      } catch (error) {
+        setHasError(true);
       }
+      setIsLoading(false);
     };
     fetchQuote();
   }, []);
 
-  return <section className="quoteContainer">{quote}</section>;
+  return (
+    <section className="quoteContainer">
+      {isLoading && 'Loading..'}
+      {hasError && 'Ups! Something went wrong...'}
+      {quote}
+    </section>
+  );
 };
 
 export default Quote;
